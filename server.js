1d3288c66e6d3796749a8646f9f0ca8b00fbc579
Nelson <nelson.h.labrin8@gmail.com>
2025-11-22 18:14:11 -0300

    Modificacion en server.js para la conexion a la DB en MySQL y la creacion de sus tablas

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const mysql = require('mysql');
 
 const app = express();
 const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '62}91Adc_.c5',
+    database: 'calendario_de_riego'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos: ' + err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos MySQL como id ' + db.threadId);
+});
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -19,3 +35,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor iniciando en http://localhost:${port}`);
 });
+
